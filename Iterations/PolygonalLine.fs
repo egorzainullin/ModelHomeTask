92f10c640cc043375079e97042fc4834bf395b6a
egorzainullin <egor.zainullin@icloud.com>
2021-01-05 19:38:53 +0300

    fixed line

--- Iterations/PolygonalLine.fs
+++ Iterations/PolygonalLine.fs
@@ -53,7 +53,7 @@ module PolygonalLine =
                 let d = (a - b).Length()
                 let extraDelta = d - h
                 if extraDelta < (float32) 0.0 then
-                     transform' h (b :: proc) (a :: proc)
+                     transform' h (b :: tail) (a :: proc)
                 else
                     let c = (a + b) / (float32) 2.0
                     transform' h (b :: tail) ((c :: a :: proc))
