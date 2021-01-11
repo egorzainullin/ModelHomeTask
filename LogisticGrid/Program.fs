9956c8c43edfc4169b788a126a8ed5285bd68e74
egorzainullin <egor.zainullin@icloud.com>
2021-01-11 09:45:09 +0300

    added view for grid

--- LogisticGrid/Program.fs
+++ LogisticGrid/Program.fs
@@ -1,24 +1,30 @@
 ﻿open LogisticGrid
 open XPlot.Plotly
-
+type Drawing(gn, cp, iterNumber) =
+    class
+        let mutable array = Model.arr
+        
+        member this.Draw() =
+            for i in [1..iterNumber] do
+                array <- Model.calculateNewState (gn, cp, array)
+                let (xs1, ys1) = List.unzip Model.list1
+                let (xs2, ys2) = List.unzip Model.list2
+                let (xs3, ys3) = List.unzip Model.list3
+                let scatter1 = Charting.toStyledScatter xs1 ys1 "blue"
+                let scatter2 = Charting.toStyledScatter xs2 ys2 "yellow"
+                let scatter3 = Charting.toStyledScatter xs3 ys3 "green"
+                let chart =
+                    [ scatter1; scatter2; scatter3 ]
+                    |> Chart.Plot
+                    |> Chart.WithHeight 500
+                    |> Chart.WithWidth 500
+                if (i = iterNumber) then Chart.Show chart
+    end
 
 [<EntryPoint>]
 let main argv =
     let gn = 251.0
     let cp = 999.0
-    let mutable array = Model.arr
-    array <- Model.calculateNewState (gn, cp, array)
-    let (xs1, ys1) = List.unzip Model.list1
-    let (xs2, ys2) = List.unzip Model.list2
-    let (xs3, ys3) = List.unzip Model.list3
-    let scatter1 = Charting.toStyledScatter xs1 ys1 "blue"
-    let scatter2 = Charting.toStyledScatter xs2 ys2 "yellow"
-    let scatter3 = Charting.toStyledScatter xs3 ys3 "green"
-    let chart =
-            [ scatter1; scatter2; scatter3 ]
-            |> Chart.Plot
-            |> Chart.WithHeight 500
-            |> Chart.WithWidth 500
-    Chart.Show chart
-    printfn "%A" argv
+    
+    
     0 // return an integer exit code
---
+++ LogisticGridDisplay/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LogisticGrid;
+
+namespace LogisticGridDisplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessage(string message)
+        {
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Double.TryParse(textBox1.Text, out var gn) && (Double.TryParse(textBox2.Text, out var cp)))
+            {
+                if (int.TryParse(textBox3.Text, out var iterNumber))
+                {
+                    var drawing = new global::Program.Drawing(gn, cp, iterNumber);
+                    drawing.Draw();
+                }
+                
+            }
+            else
+            {
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+    }
+}
